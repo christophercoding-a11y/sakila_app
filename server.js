815e753d488c81360947174cd5fbaea7d12bb9ad
christophercodinga11y <christopherjack14@gmail.com>
2025-04-03 14:09:24 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+// 1. Import express and create a server instance and port
+const express = require('express')
+const server = express()
+const port = process.env.port || 3000
+
+// import router
+const router = require('./app/routes/router')
+const { default: helmet } = require('helmet')
+
+// increase security using helmet
+server.use(helmet()).use(express.json()).use('/', router)
+
+// Root Route
+// .get(path, callback function)
+server.get('/api', (req, res)=> {
+    res.json({
+        'All Actors': `http://localhost:${port}/api/actor`,
+        'All Customers': `http://localhost:${port}/api/customer`,
+        'All Films': `http://localhost:${port}/api/film`,
+    })
+})
+// 2. Listen on the port
+// .listen(port, callback function)
+server.listen(port, ()=> console.log(`Let's get this PORTY started in room ${port}...`))
+
